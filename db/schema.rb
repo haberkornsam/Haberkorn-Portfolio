4cffe48c8b4ad87733b3d51c4b9ef5075125a744
Sam Haberkorn <haberkornsam@gmail.com>
2021-05-09 04:39:40 +0000

    ***subtly breaks everything***

--- db/schema.rb
+++ db/schema.rb
@@ -12,47 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2021_05_09_042707) do
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.text "description"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["slug"], name: "index_posts_on_slug", unique: true
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.text "description"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["slug"], name: "index_projects_on_slug", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
---
+++ spec/factories/projects.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :project do
+    title { "MyString" }
+    body { "MyText" }
+    description { "MyText" }
+    slug { "MyString" }
+  end
+end
